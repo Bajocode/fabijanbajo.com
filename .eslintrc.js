141d9dcf6408dc59793676d94d6fc86b30855c15
Fabijan Bajo <bajo09@gmail.com>
2019-06-19 07:19:31 +0200

    Add linting and formatting options

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports =  {
+  parser: '@typescript-eslint/parser',
+  extends: ["airbnb-typescript"],
+  parserOptions:  {
+    ecmaVersion: 2018,  // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module',  // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+      tsx: true
+    },
+  },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  settings: {
+    react: {
+      version: 'detect',  // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
+};
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-const Navigation: React.FC = () => {
-  return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        
-      </nav>
-    </div>
-  )
-}
+const Navigation: React.FC = () => (
+  <div>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top" />
+  </div>
+);
 
 export default Navigation;
