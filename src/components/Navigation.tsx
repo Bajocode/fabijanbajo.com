9565083523e09f07860c73b59e0addecbe27240f
Fabijan Bajo <bajo09@gmail.com>
2019-06-19 06:37:00 +0200

    Add about and navigation

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation: React.FC = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        
+      </nav>
+    </div>
+  )
+}
+
+export default Navigation;
