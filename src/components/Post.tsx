8c4e9af6277205a51e65491652a66cab3c6ec38b
Fabijan Bajo <bajo09@gmail.com>
2019-07-06 21:04:15 +0200

    Update html title

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,34 +1,7 @@
 import React from 'react';
-import ReactMarkdown from 'react-markdown';
 
 const Post: React.FC = () => {
-
-  const testPost = `
-    # First blog
-
-    ## Intro
-    Hello world here is how I will fill this page up
-    
-    
-    \`\`\` Swift
-    let ball = 4
-    
-    print("Hello world")
-    
-    func returnSome() -> Int {
-      return 500
-    }
-    \`\`\`
-    
-    > Just foolinf around
-    to test this out!!!!!😋
-  `;
-
-  return (
-    <div className="bg-light">
-      <ReactMarkdown source={testPost} />
-    </div>
-  );
+  
 };
 
 export default Post;
