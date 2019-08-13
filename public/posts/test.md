8c5fdf0d81a577e4c02a90f39eaa69fc339a2e37
Fabijan Bajo <bajo09@gmail.com>
2019-08-13 08:36:14 +0200

    Create markdown file reading flow

---
+++ public/posts/test.md
@@ -0,0 +1,9 @@
+# Hello world
+
+## My story
+I will go and take over fucking America
+
+```typescript
+
+const testingSHIT = 4 + 2;
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import { useRoutes } from 'hookrouter';
-import Home from './components/Home';
+// import Home from './components/Home';
+import Post from './components/Post';
 import NavBar from './components/Navbar';
 import Footer from './components/Footer';
 
-const routes = {
-  '/': () => <Home />,
-};
+const App: React.FC = () => {
+  const [post, setPost] = React.useState('');
+  const routes = {
+    '/': () => <Post title="" body={post} />,
+  };
 
-const App: React.FC = () => (
-  <div className="container pt-4 mt-4">
-    <NavBar />
-    {useRoutes(routes)}
-    <Footer />
-  </div>
-);
+  React.useEffect(() => {
+    fetch('posts/test.md')
+      .then(res => res.text())
+      .then(text => setPost(text));
+  });
+
+  return (
+    <div className="container pt-4 mt-4">
+      <NavBar />
+      {useRoutes(routes)}
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
